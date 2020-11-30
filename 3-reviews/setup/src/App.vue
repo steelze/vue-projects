7502cdda8a43a3ee3a8676fcae3e505a6f7abfea
Odunayo Ogungbure <sogungbure@gmail.com>
2020-11-30 16:20:58 +0100

    3-reviews

---
+++ 3-reviews/setup/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Review />
+</template>
+
+<script>
+import Review from '@/components/Review.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Review,
+  },
+}
+</script>
---
+++ 3-reviews/setup/src/components/Review.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>review component</h2>
+</template>
+
+<script>
+export default {
+  name: 'Review',
+}
+</script>
