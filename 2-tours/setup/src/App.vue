2fd5f50596363d175f72f12004f63e2f2e904773
Odunayo Ogungbure <sogungbure@gmail.com>
2020-11-30 15:26:57 +0100

    2-tours

---
+++ 2-tours/setup/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Loading />
+  <Tour />
+</template>
+
+<script>
+import Loading from '@/components/Loading.vue';
+import Tour from '@/components/Tour.vue';
+
+// const url = 'https://course-api.com/react-tours-project';
+
+export default {
+  name: 'App',
+  components: {
+    Loading,
+    Tour,
+  },
+}
+</script>
---
+++ 2-tours/setup/src/components/Tour.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Tour</h1>
+</template>
+
+<script>
+export default {
+  name: 'Tour',
+}
+</script>
