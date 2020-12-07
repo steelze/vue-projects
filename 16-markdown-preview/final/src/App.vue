138d86d61b9d9cbda147575a96eba780a89708a8
Odunayo Ogungbure <sogungbure@gmail.com>
2020-12-07 14:28:51 +0100

    16-markdown-preview

---
+++ 16-markdown-preview/final/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <section class='markdown'>
+      <textarea class="input" v-model="input"></textarea>
+      <article class="result" v-html="markdown"></article>
+    </section>
+  </main>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+import marked from 'marked';
+
+export default {
+  name: 'App',
+  setup() {
+    const input = ref('# markdown preview');
+    const markdown = computed(() => marked(input.value));
+
+    return { input, markdown };
+  },
+}
+</script>
---
+++ 16-markdown-preview/setup/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>markdown preview starter</h2>
+</template>
+
+<script>
+import marked from 'marked';
+
+export default {
+  name: 'App',
+}
+</script>
